530ee65789f563f7daa58db4e53365e1b851a2cf
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2020-04-06 15:49:14 +0100

    Initial setup, and create first get request to scrape data

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url =
+  "http://www.nfl.com/stats/team?seasonId=2019&seasonType=REG&Submit=Go";
 
 const HomePage = () => {
+  useEffect(() => {
+    console.log("Home page mounted ------------------>");
+
+    axios
+      .get(proxyurl + url)
+      .then((res) => {
+        const html = res.data;
+        const $ = cheerio.load(html);
+        const leagueLeadersOffense = $(".col:nth-child(1) > .data-table1");
+
+        const leagueLeadersOffenseArray = [];
+
+        leagueLeadersOffense.each(function () {
+          const statisticTitle = $(this).find("thead td:nth-child(1)").text();
+          const fiveTeamsRows = $(this).find("tbody tr");
+
+          const fiveTeamsArray = [];
+          fiveTeamsRows.each(function () {
+            const teamName = $(this).find("td[scope='row'] a").text();
+            const teamStat = $(this).find("td[align='right']").text();
+
+            fiveTeamsArray.push({ teamName, teamStat });
+          });
+
+          leagueLeadersOffenseArray.push({ statisticTitle, fiveTeamsArray });
+        });
+
+        console.log(leagueLeadersOffenseArray);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div>
-      <h1>React Slingshot</h1>
-
-      <h2>Get Started</h2>
-      <ol>
-        <li>Review the <Link to="/fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
-      </ol>
+      <h1>NFL Stats Scraper</h1>
     </div>
   );
 };
